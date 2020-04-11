1260b05a0c79e697f42a3ba462d8cfcf4445008f
Florian Jäger <florian.jaeger@students.fhnw.ch>
2020-04-11 16:42:33 +0200

    Copied classes from poker game (Bradley)

--- src/client_v0/ClientViewManager.java
+++ src/client_v0/ClientViewManager.java
@@ -1,8 +1,5 @@
 package client_v0;
 
-
-
-
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;;
---
+++ src/jassmendPackage/Player.java
@@ -0,0 +1,35 @@
+package jassmendPackage;
+
+import java.util.ArrayList;
+
+public class Player  {
+    public static final int HAND_SIZE = 5;
+    
+    private final String playerName; // This is the ID
+    private final ArrayList<Card> cards = new ArrayList<>();
+  
+    public Player(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+    
+    public void addCard(Card card) {
+        if (cards.size() < HAND_SIZE) cards.add(card);
+    }
+    
+    public void discardHand() {
+        cards.clear();
+       
+    }
+    
+    public int getNumCards() {
+        return cards.size();
+    }
+}
