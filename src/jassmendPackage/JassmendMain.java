bb10e7a4cb673a8e6e711e4eaa1892256335160c
Florian Jäger <florian.jaeger@students.fhnw.ch>
2020-04-11 17:42:26 +0200

    JassmendMain und Controller
    Some changes

--- src/jassmendPackage/JassmendMain.java
+++ src/jassmendPackage/JassmendMain.java
@@ -1,10 +1,24 @@
 package jassmendPackage;
 
-public class JassmendMain {
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		
 
-	}
+public class JassmendMain extends Application {
+	public static final int NUM_PLAYERS = 2;
+	JassmendModel model;
+	JassmendView view;
+	JassmendController controller;
+	
+    public static void main(String[] args) {
+        launch();
+    }
 
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    	// Create and initialize the MVC components
+    	model = new JassmendModel();
+    	view = new JassmendView(primaryStage, model);
+    	controller = new JassmendController(model, view);
+    }
 }
