f3e08fecee16657e17198e8e0de2ff3362fe3919
Gazmend Shefiu <gazmend_shefiu@hotmail.com>
2020-05-07 20:45:40 +0200

    DB correction

--- src/jassmendView/JassmendGameView.java
+++ src/jassmendView/JassmendGameView.java
@@ -28,9 +28,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 
-import jassmendPackage.Player;
-import jassmendPackage.Card;
-import jassmendPackage.CardView;
 // import jassmendPackage.CardHand;
 import jassmendMain.JassmendMain;
 import jassmendModel.Card;
